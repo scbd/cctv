e10e1d0c24b9f112fe2c6a9f66d67d52395b82a1
Stephane Bilodeau <bilodeaustephane@gmail.com>
2018-11-10 18:29:36 +0200

    Show schedule.title in priority + increate description size

--- app/views/help/event-information.js
+++ app/views/help/event-information.js
@@ -7,7 +7,7 @@ define(['app', 'services/cctv-stream', 'services/caches'], function() { "use str
         cctvStream.initialize().then(function(){
 
             _ctrl.eventGroup   = cctvStream.event;
-            _ctrl.title        = cctvStream.event.Title.en.replace(/-/g, '‑') //replace - by a non-breaking one
+            _ctrl.title        = ((cctvStream.event.schedule||{}).title       || cctvStream.event.Title.en).replace(/-/g, '‑') //replace - by a non-breaking one
             _ctrl.descriptions = ((cctvStream.event.schedule||{}).description || cctvStream.event.Description.en).split('\n');
 
         });
