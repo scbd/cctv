8f2fae17752658738334e48edc9f1bfa99576fb1
Stephane <bilodeaustephane@gmail.com>
2021-12-22 09:00:42 -0500

    Use `base_url` http header (rollback) when no BASE_PATH env var provided

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const express = require('express');
 const app     = express();
 const proxy   = require('http-proxy').createProxyServer({});
 const apiUrl  = process.env.API_URL || 'https://api.cbd.int:443';
-const basePath = path.join('/', process.env.BASE_PATH||'/', '/');
+const basePath = process.env.BASE_PATH ? toBasePath(process.env.BASE_PATH) : null;
 
 if(!process.env.API_URL)   console.error(`WARNING: environment API_URL not set. USING default`);
-if(!process.env.BASE_PATH) console.error(`WARNING: environment BASE_PATH not set. USING default`);
+if(!process.env.BASE_PATH) console.error(`WARNING: environment BASE_PATH not set. USING 'base_url' http header or '/' as default`);
 
 console.log('API url: ', apiUrl);
-console.log(`Base path: ${basePath}`);
+console.log(`Base path: ${basePath||"'base_url' http header or '/'"}`);
 
 app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
@@ -28,13 +28,13 @@ const appRoutes = new express.Router();
 appRoutes.use('/app',      express.static(__dirname + '/app'));
 appRoutes.use('/app/libs', express.static(__dirname + '/node_modules/@bower_components' ));
 appRoutes.all('/app/*',    (req, res) => res.status(404).send());
-appRoutes.get('/*',        (req, res) => res.render('template', { baseUrl: basePath }));
+appRoutes.get('/*',        (req, res) => { 
+    const baseUrl = toBasePath(basePath || req.headers.base_url);
+    res.render('template', { baseUrl: urlSafe(baseUrl) })
+});
 
 // START SERVER
-
-app.use(basePath, appRoutes);
-
-if(basePath!='/') app.use('/', appRoutes); // temps support for transition to Traefik 2 / AWS ALB
+app.use(toBasePath(basePath), appRoutes);
 
 app.listen(process.env.PORT || 2000, function () {
 	console.log(`Server listening on ${this.address().port}`);
@@ -46,3 +46,12 @@ proxy.on('error', function (e,req, res) {
     console.error('proxy error:', e);
     res.status(502).send();
 });
+
+function toBasePath(dir) {
+    return path.join('/', dir || '/', '/');
+}
+
+function urlSafe(dir) {
+    var parts = dir.split('/').map(o=>encodeURIComponent(o));
+    return path.join.apply(null, parts.map(o=>o||'/'));
+}
