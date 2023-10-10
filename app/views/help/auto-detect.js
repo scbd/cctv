467eb601f534b7036102b768bd38b68bfbeffa8d
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2023-10-10 22:55:25 +0300

    Support cctv/current stream id to distinguish from Web version

--- app/views/help/auto-detect.js
+++ app/views/help/auto-detect.js
@@ -3,17 +3,27 @@ define(['app'], function() { "use strict";
 	return ['$http', '$location', function($http, $location) {
 
         $http.get('/api/v2016/conferences', { params: {
-            q: { 
-                "conference.streamId":{ "$exists":true}, 
+            q: {
+                $or: [ 
+                    { "conference.streamId"           :{ "$exists": true } }, 
+                    { "conference.defaultCctvStreamId":{ "$exists": true } }, 
+                ],
                 "active" :true
             },
-            f:  { "conference.streamId" : 1, "EndDate":1 },
+            f:  { "conference.streamId" : 1, "conference.defaultCctvStreamId" : 1, "EndDate":1 },
             s:  { "EndDate": -1 },
             fo: 1
         }}).then(function(res){
-            var streamId = res.data.conference.streamId
+            var conference = res.data.conference;
+            var streamId   = conference.defaultCctvStreamId || conference.streamId;
 
-            $location.search({streamId: streamId })
+            if(!streamId) throw new Error("No streamId found");
+
+            var queryStringParams = $location.search() || {};
+
+            queryStringParams.streamId = streamId;
+
+            $location.search(queryStringParams)
             $location.path('/');
 
         }).catch(function(err){
